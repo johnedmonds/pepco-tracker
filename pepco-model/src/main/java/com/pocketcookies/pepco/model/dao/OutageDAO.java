53fbb3b3fba2fe080173c848599959a80e34f1fe
John Edmonds <john.a.edmonds@gmail.com>
2011-07-13 22:23:45 -0400

    Added a function to retrieve summaries from the database and an associated test.

--- pepco-model/src/main/java/com/pocketcookies/pepco/model/dao/OutageDAO.java
+++ pepco-model/src/main/java/com/pocketcookies/pepco/model/dao/OutageDAO.java
@@ -1,10 +1,13 @@
 package com.pocketcookies.pepco.model.dao;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
 
 import com.pocketcookies.pepco.model.Outage;
+import com.pocketcookies.pepco.model.Summary;
 
 public class OutageDAO {
 	private final SessionFactory sessionFactory;
@@ -26,5 +29,39 @@ public class OutageDAO {
 		if (outages.isEmpty())
 			return null;
 		return outages.get(0);
+	}
+
+	/**
+	 * Gets summaries ordered by when they were generated occurring after [from]
+	 * and before [to] inclusive.
+	 * 
+	 * Note that this function will not reorder [from] and [to]. If [to] is
+	 * before [from], no results will be returned.
+	 * 
+	 * @param from
+	 *            Only summaries generated after this date/time will be
+	 *            returned.
+	 * @param to
+	 *            Only summaries before this date/time will be returned.
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Summary> getSummaries(Timestamp from, Timestamp to) {
+		// If the caller did not specify [from], we want to return all Summaries
+		// from as early as possible. Thus, we pick a safe date, before which
+		// there were no summaries. We started collecting data in 2011, so
+		// picking 1970 is a safe (and easy) choice for [from].
+		if (from == null)
+			from = new Timestamp(0);
+		// If the caller did not specify [to], we want to return all summaries
+		// including the newest ones. Since there can be no summaries in the
+		// future, we will pick now as a good default [to].
+		if (to == null)
+			to = new Timestamp(new Date().getTime());
+		return this.sessionFactory
+				.getCurrentSession()
+				.createQuery(
+						"from Summary where whenGenerated >= :from and whenGenerated <= :to")
+				.setTimestamp("from", from).setTimestamp("to", to).list();
 	}
 }
---
+++ pepco-model/src/test/java/com/pocketcookies/pepco/model/TestOutageDAO.java
@@ -0,0 +1,47 @@
+package com.pocketcookies.pepco.model;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.pocketcookies.pepco.model.dao.OutageDAO;
+
+public class TestOutageDAO extends TestCase {
+	private SessionFactory sessionFactory;
+
+	public void setUp() {
+		this.sessionFactory = new Configuration().configure()
+				.buildSessionFactory();
+	}
+
+	public void testGetSummaries() {
+		final OutageDAO dao = new OutageDAO(this.sessionFactory);
+		// Set up data.
+		Session session = this.sessionFactory.getCurrentSession();
+		session.beginTransaction();
+		Summary s1 = new Summary(1, 1, 1, 1, 1, 1, 1, new Timestamp(1), null);
+		Summary s2 = new Summary(1, 1, 1, 1, 1, 1, 1, new Timestamp(2), null);
+		session.save(s1);
+		session.save(s2);
+		session.getTransaction().commit();
+		session = this.sessionFactory.getCurrentSession();
+		session.beginTransaction();
+		s1 = (Summary) session.load(Summary.class, s1.getId());
+		s2 = (Summary) session.load(Summary.class, s2.getId());
+		System.out.println(s1.getWhenGenerated().getTime());
+		assertEquals(s2, dao.getSummaries(new Timestamp(2), new Timestamp(2))
+				.get(0));
+		assertEquals(s1, dao.getSummaries(new Timestamp(0), new Timestamp(1))
+				.get(0));
+		assertEquals(s1, dao.getSummaries(null, new Timestamp(1)).get(0));
+		assertEquals(s1, dao.getSummaries(new Timestamp(1), null).get(0));
+		assertEquals(2, dao.getSummaries(null, null).size());
+		assertTrue(dao.getSummaries(null, null).contains(s1));
+		assertTrue(dao.getSummaries(null, null).contains(s2));
+	}
+}

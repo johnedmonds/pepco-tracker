1d1ef0634490bc4b1e258f153208faf3ff154a8b
John Edmonds <john.a.edmonds@gmail.com>
2011-07-16 15:11:13 -0400

    Adding files necessary to display the view.

---
+++ pepco-web/src/main/java/com/pocketcookies/pepco/web/PepcoController.java
@@ -0,0 +1,36 @@
+package com.pocketcookies.pepco.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.pocketcookies.pepco.model.Summary;
+import com.pocketcookies.pepco.model.dao.OutageDAO;
+
+@Controller
+public class PepcoController {
+	private final OutageDAO outageDao;
+
+	@Autowired
+	public PepcoController(@Qualifier("outageDao") final OutageDAO outageDao) {
+		this.outageDao = outageDao;
+	}
+
+	@RequestMapping(value = "/")
+	public ModelAndView index() {
+		final List<Summary> summaries = this.outageDao.getSummaries(null, null,
+				true, 2);
+		final Summary currentSummary = summaries.get(0);
+		// final Summary previousSummary = summaries.get(1);
+		final ModelAndView mav = new ModelAndView("pepco.homepage");
+		mav.addObject("totalOutages", currentSummary.getTotalOutages());
+		mav.addObject("dcCust", currentSummary.getDcAffectedCustomers());
+		mav.addObject("pgCust", currentSummary.getPgAffectedCustomers());
+		mav.addObject("montCust", currentSummary.getMontAffectedCustomers());
+		return mav;
+	}
+}

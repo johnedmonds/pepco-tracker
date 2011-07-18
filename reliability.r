524878429688d0aeeeecb6622f55cda520d1b27f
John Edmonds <john.a.edmonds@gmail.com>
2011-07-18 19:19:58 -0400

    Added some comments and removed the grey padding.

--- reliability.r
+++ reliability.r
@@ -4,7 +4,11 @@ reliability <- read.csv(stdin())
 reliability$observed <- as.Date(reliability$observed, format="%Y-%m-%d %H:%M:%S")
 
 areas <- factor(reliability$area)
+#Make the plot.
 reliability_plot <- ggplot(reliability, aes(x=observed, y=outages))
 reliability_plot <- reliability_plot + geom_area(aes(fill=areas))
+#Remove the grey padding.
+reliability_plot <- reliability_plot + scale_x_date(expand=c(0,0))
+reliability_plot <- reliability_plot + scale_y_continuous(expand=c(0,0))
 #reliability_plot <- reliability_plot + scale_fill_manual()
 ggsave(reliability_plot,filename='reliability.png')

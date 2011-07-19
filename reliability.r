96de129be05398d820edc56e020f7e5166cc3294
John Edmonds <john.a.edmonds@gmail.com>
2011-07-18 20:00:56 -0400

    Updated the script to make the plot shorter and successfully read from stdin.

--- reliability.r
+++ reliability.r
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-reliability <- read.csv(stdin())
+reliability <- read.csv('/dev/stdin')
 reliability$observed <- as.Date(reliability$observed, format="%Y-%m-%d %H:%M:%S")
 
 areas <- factor(reliability$area)
@@ -10,5 +10,7 @@ reliability_plot <- reliability_plot + geom_area(aes(fill=areas))
 #Remove the grey padding.
 reliability_plot <- reliability_plot + scale_x_date(expand=c(0,0))
 reliability_plot <- reliability_plot + scale_y_continuous(expand=c(0,0))
+#reliability_plot <- reliability_plot + opts(legend.position=c(.5,.5))
 #reliability_plot <- reliability_plot + scale_fill_manual()
-ggsave(reliability_plot,filename='reliability.png')
+reliability_dpi<-72
+ggsave(reliability_plot,filename='reliability.svg', width=900/reliability_dpi, height=150/reliability_dpi, dpi=reliability_dpi)

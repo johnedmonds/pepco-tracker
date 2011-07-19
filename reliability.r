f479a6718dd8e1f5630f662367a3edf87a480b42
John Edmonds <john.a.edmonds@gmail.com>
2011-07-18 22:15:49 -0400

    Updated labels on the plot.

--- reliability.r
+++ reliability.r
@@ -3,13 +3,16 @@ library(ggplot2)
 reliability <- read.csv('/dev/stdin')
 reliability$observed <- as.Date(reliability$observed, format="%Y-%m-%d %H:%M:%S")
 
-areas <- factor(reliability$area)
+Areas <- factor(reliability$area)
 #Make the plot.
 reliability_plot <- ggplot(reliability, aes(x=observed, y=outages))
-reliability_plot <- reliability_plot + geom_area(aes(fill=areas))
+reliability_plot <- reliability_plot + geom_area(aes(fill=Areas))
 #Remove the grey padding.
-reliability_plot <- reliability_plot + scale_x_date(expand=c(0,0))
-reliability_plot <- reliability_plot + scale_y_continuous(expand=c(0,0))
+reliability_plot <- reliability_plot + scale_x_date('Time', expand=c(0,0))
+reliability_plot <- reliability_plot + scale_y_continuous('Customers Without Power', expand=c(0,0))
+
+#Axis/title labeling
+reliability_plot <- reliability_plot + opts(title='Pepco Reliability Statistics')
 #reliability_plot <- reliability_plot + opts(legend.position=c(.5,.5))
 #reliability_plot <- reliability_plot + scale_fill_manual()
 reliability_dpi<-72

cda2c97cfe27d35c1274a52a558837f7537130fb
John Edmonds <john.a.edmonds@gmail.com>
2011-07-31 13:52:32 -0400

    Moved S3Uploader into its own project.

--- pepco-uploader/src/main/java/com/pocketcookies/pepco/uploader/S3Uploader.java
+++ pepco-uploader/src/main/java/com/pocketcookies/pepco/uploader/S3Uploader.java
@@ -1,4 +1,4 @@
-package com.pocketcookies.s3;
+package com.pocketcookies.pepco.uploader;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,12 +22,12 @@ public class S3Uploader {
 		this.bucket = bucket;
 		this.websiteRoot = websiteRoot;
 	}
-	
+
 	public void upload(String url) throws IOException {
 		URL urlToDownload = new URL(websiteRoot + "/" + url);
 		URLConnection conn = urlToDownload.openConnection();
 		InputStream is = urlToDownload.openStream();
-		
+
 		ObjectMetadata metadata = new ObjectMetadata();
 		metadata.setContentLength(conn.getContentLength());
 		metadata.setContentType(conn.getContentType());
@@ -35,13 +35,14 @@ public class S3Uploader {
 		req.setCannedAcl(CannedAccessControlList.PublicRead);
 		s3Client.putObject(req);
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		String accessKeyID = args[0];
 		String secretKeyID = args[1];
 		String websiteRoot = args[2];
-		
-		S3Uploader s3Uploader = new S3Uploader(new AmazonS3Client(new BasicAWSCredentials(accessKeyID, secretKeyID)),
+
+		S3Uploader s3Uploader = new S3Uploader(new AmazonS3Client(
+				new BasicAWSCredentials(accessKeyID, secretKeyID)),
 				"pepco-web", websiteRoot);
 		s3Uploader.upload("summary-data");
 		s3Uploader.upload("index.html");

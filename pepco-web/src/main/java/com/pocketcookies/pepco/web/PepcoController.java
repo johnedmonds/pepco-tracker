90aea0380a1d23b63e1855d2727db3ff86d796fc
John Edmonds <john.a.edmonds@gmail.com>
2011-07-30 20:17:58 -0400

    PepcoController can now return JSON for the chart to pick up.

--- pepco-web/src/main/java/com/pocketcookies/pepco/web/PepcoController.java
+++ pepco-web/src/main/java/com/pocketcookies/pepco/web/PepcoController.java
@@ -2,10 +2,15 @@ package com.pocketcookies.pepco.web;
 
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.pocketcookies.pepco.model.Summary;
@@ -29,5 +34,29 @@ public class PepcoController {
 		final ModelAndView mav = new ModelAndView("pepco.homepage");
 		mav.addObject("summary", currentSummary);
 		return mav;
+	}
+
+	@RequestMapping(value = "/summary-data", method = RequestMethod.GET)
+	@ResponseBody
+	public String summary() throws JSONException {
+		final JSONArray dcdata = new JSONArray(), pgdata = new JSONArray(), montdata = new JSONArray();
+		final JSONObject dc = new JSONObject().put("label", "DC").put("data",
+				dcdata);
+		final JSONObject pg = new JSONObject().put("label", "Prince George")
+				.put("data", pgdata);
+		final JSONObject mont = new JSONObject().put("label", "Montgomery")
+				.put("data", montdata);
+
+		for (final Summary s : this.summaryDao.getSummaries(null, null, false,
+				0)) {
+			dcdata.put(new JSONArray().put(s.getWhenGenerated().getTime()).put(
+					s.getDcAffectedCustomers()));
+			pgdata.put(new JSONArray().put(s.getWhenGenerated().getTime()).put(
+					s.getPgAffectedCustomers()));
+			montdata.put(new JSONArray().put(s.getWhenGenerated().getTime())
+					.put(s.getMontAffectedCustomers()));
+		}
+
+		return new JSONArray().put(dc).put(pg).put(mont).toString();
 	}
 }

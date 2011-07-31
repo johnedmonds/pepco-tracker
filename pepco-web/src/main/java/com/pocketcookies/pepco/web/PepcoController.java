9ba593b2aff44e35ceb31768b0d5892ea38fc0bd
John Edmonds <john.a.edmonds@gmail.com>
2011-07-31 18:05:13 -0400

    Added CSV.

--- pepco-web/src/main/java/com/pocketcookies/pepco/web/PepcoController.java
+++ pepco-web/src/main/java/com/pocketcookies/pepco/web/PepcoController.java
@@ -1,5 +1,10 @@
 package com.pocketcookies.pepco.web;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,7 +30,7 @@ public class PepcoController {
 
 	@RequestMapping(value = "/")
 	public ModelAndView index() {
-		final Summary summary= this.summaryDao.getMostRecentSummary();
+		final Summary summary = this.summaryDao.getMostRecentSummary();
 		// final Summary previousSummary = summaries.get(1);
 		final ModelAndView mav = new ModelAndView("pepco.homepage");
 		mav.addObject("summary", summary);
@@ -54,5 +59,27 @@ public class PepcoController {
 		}
 
 		return new JSONArray().put(dc).put(pg).put(mont).toString();
+	}
+
+	@RequestMapping(value = "/summary-csv", method = RequestMethod.GET)
+	public void summaryCsv(final HttpServletResponse response)
+			throws IOException {
+		response.setContentType("text/csv");
+		final StringBuilder sb = new StringBuilder(
+				"Observation Timestamp,DC Affected,PG Affected,Montgomery Affected,DC Total,PG Total,Montgomery Total\r\n");
+		final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		for (final Summary s : this.summaryDao.getSummaries(null, null, false,
+				0)) {
+			sb.append(sdf.format(s.getWhenGenerated())).append(",")
+					.append(s.getDcAffectedCustomers()).append(",")
+					.append(s.getPgAffectedCustomers()).append(",")
+					.append(s.getMontAffectedCustomers()).append(",")
+					.append(s.getDcTotalCustomers()).append(",")
+					.append(s.getPgTotalCustomers()).append(",")
+					.append(s.getMontTotalCustomers()).append("\r\n");
+		}
+		response.setContentLength(sb.length());
+		response.getOutputStream().write(sb.toString().getBytes());
+		response.getOutputStream().close();
 	}
 }

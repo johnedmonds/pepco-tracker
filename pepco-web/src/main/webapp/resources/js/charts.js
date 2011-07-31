555396162e1cbef026a73d5fc8fbb522ced0f0d7
John Edmonds <john.a.edmonds@gmail.com>
2011-07-31 13:22:53 -0400

    The chart's y axis is now formatted with a thousands separator.

--- pepco-web/src/main/webapp/resources/js/charts.js
+++ pepco-web/src/main/webapp/resources/js/charts.js
@@ -1,3 +1,15 @@
+//Copied from http://www.mredkj.com/javascript/numberFormat.html
+function addCommas(nStr) {
+	nStr += '';
+	x = nStr.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
+}
 function makeChart(dataset) {
 	$.plot($("#reliability-chart-placeholder"), dataset, {
 		series : {
@@ -8,8 +20,13 @@ function makeChart(dataset) {
 			}
 		},
 		xaxis : {
+			show : true,
 			mode : "time",
-			timeformat : "%y-%m-%d"
+			timeformat : "%y-%m-%d",
+		},
+		yaxis : {
+			show : true,
+			tickFormatter : addCommas
 		}
 	});
 	$(".tickLabels").append("<div class=\"tickLabel xAxisLabel\">Time</div>");

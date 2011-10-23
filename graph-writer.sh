32a5dc2d5f89791baca7af0978cb42585ddf2e45
John Edmonds <john.a.edmonds@gmail.com>
2011-10-23 15:24:49 -0400

    Take parameters from the command line.

--- graph-writer.sh
+++ graph-writer.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-PEPCO_DATABASE_NAME="pepco"
-$POSTGRES_HOME/bin/psql --no-align --field-separator "," -d $PEPCO_DATABASE_NAME --file summary-query.sql --pset footer | Rscript reliability.r test.svg
+PEPCO_DATABASE_NAME=$1
+USERNAME=$2
+OUTPUT_DIR=$3
+$POSTGRES_HOME/bin/psql --no-align --field-separator "," -d $PEPCO_DATABASE_NAME -U $USERNAME --file summary-query.sql --pset footer | Rscript reliability.r $OUTPUT_DIR/test.svg
 

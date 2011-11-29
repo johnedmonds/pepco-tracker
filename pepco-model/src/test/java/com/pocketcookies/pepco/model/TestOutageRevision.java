ed1f5abed40c53d70b67c37e8eb4d800e1cdf179
John Edmonds <john.a.edmonds@gmail.com>
2011-11-29 16:18:32 -0500

    Add method to compare AbstractOutageRevisions for sorting.

---
+++ pepco-model/src/test/java/com/pocketcookies/pepco/model/TestOutageRevision.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pocketcookies.pepco.model;
+
+import java.sql.Timestamp;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author m1jae02
+ */
+public class TestOutageRevision extends TestCase {
+
+    /**
+     * Tests compareTo
+     */
+    public void testCompareTo() {
+	assertEquals(0,
+		new OutageClusterRevision(1, new Timestamp(1), null, new ParserRun(new Timestamp(1), new Timestamp(1)), 1).compareTo(
+		new OutageClusterRevision(1, new Timestamp(1), null, new ParserRun(new Timestamp(1), new Timestamp(1)), 1)));
+	assertTrue(
+		new OutageClusterRevision(1, new Timestamp(1), null, new ParserRun(new Timestamp(1), new Timestamp(0)), 1).compareTo(
+		new OutageClusterRevision(1, new Timestamp(1), null, new ParserRun(new Timestamp(1), new Timestamp(1)), 1)) < 0);
+	assertTrue(
+		new OutageClusterRevision(1, new Timestamp(1), null, new ParserRun(new Timestamp(1), new Timestamp(1)), 1).compareTo(
+		new OutageClusterRevision(1, new Timestamp(1), null, new ParserRun(new Timestamp(1), new Timestamp(0)), 1)) > 0);
+    }
+}

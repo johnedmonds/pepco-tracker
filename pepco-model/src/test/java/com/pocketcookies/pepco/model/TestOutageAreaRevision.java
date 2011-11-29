fb5d6530f54a79b4c817d4b6e97374b718ef6d8d
John Edmonds <john.a.edmonds@gmail.com>
2011-11-29 14:36:13 -0500

    ParserRun now takes asof.

--- pepco-model/src/test/java/com/pocketcookies/pepco/model/TestOutageAreaRevision.java
+++ pepco-model/src/test/java/com/pocketcookies/pepco/model/TestOutageAreaRevision.java
@@ -5,15 +5,16 @@ import java.sql.Timestamp;
 import junit.framework.TestCase;
 
 public class TestOutageAreaRevision extends TestCase {
-	public void testEqualsIgnoreTime() {
-		assertTrue(new OutageAreaRevision(null, 1, new ParserRun(new Timestamp(
-				1))).equalsIgnoreTime(new OutageAreaRevision(null, 1,
-				new ParserRun(new Timestamp(1)))));
-		assertTrue(new OutageAreaRevision(null, 1, new ParserRun(new Timestamp(
-				1))).equalsIgnoreTime(new OutageAreaRevision(null, 1,
-				new ParserRun(new Timestamp(2)))));
-		assertFalse(new OutageAreaRevision(null, 2, new ParserRun(
-				new Timestamp(1))).equalsIgnoreTime(new OutageAreaRevision(
-				null, 1, new ParserRun(new Timestamp(1)))));
-	}
+
+    public void testEqualsIgnoreTime() {
+	assertTrue(
+		new OutageAreaRevision(null, 1, new ParserRun(new Timestamp(1), new Timestamp(1))).equalsIgnoreTime(
+		new OutageAreaRevision(null, 1, new ParserRun(new Timestamp(1), new Timestamp(1)))));
+	assertTrue(
+		new OutageAreaRevision(null, 1, new ParserRun(new Timestamp(1), new Timestamp(1))).equalsIgnoreTime(
+		new OutageAreaRevision(null, 1, new ParserRun(new Timestamp(2), new Timestamp(2)))));
+	assertFalse(
+		new OutageAreaRevision(null, 2, new ParserRun(new Timestamp(1), new Timestamp(1))).equalsIgnoreTime(
+		new OutageAreaRevision(null, 1, new ParserRun(new Timestamp(1), new Timestamp(1)))));
+    }
 }

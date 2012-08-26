2d2f844c043f1b372f1f7b6fcb4ceeaf9e6571c7
John Edmonds <john.a.edmonds@gmail.com>
2012-08-26 14:32:27 -0400

    updateOutages instead of updateOutage.

--- pepco-scraper/src/main/java/com/pocketcookies/pepco/scraper/OutageScraper.java
+++ pepco-scraper/src/main/java/com/pocketcookies/pepco/scraper/OutageScraper.java
@@ -1,43 +1,17 @@
 package com.pocketcookies.pepco.scraper;
 
-import java.io.IOException;
-import java.sql.Timestamp;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Set;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
 import org.apache.log4j.Logger;
-import org.joda.time.DateTime;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.TextNode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
 import com.pocketcookies.pepco.model.AbstractOutageRevision;
-import com.pocketcookies.pepco.model.Outage;
-import com.pocketcookies.pepco.model.OutageClusterRevision;
-import com.pocketcookies.pepco.model.OutageRevision;
-import com.pocketcookies.pepco.model.OutageRevision.CrewStatus;
 import com.pocketcookies.pepco.model.ParserRun;
 import com.pocketcookies.pepco.model.dao.OutageDAO;
 
@@ -76,9 +50,7 @@ public class OutageScraper implements Scraper {
 		logger.info("Starting download.");
 		final Set<AbstractOutageRevision> revisions = outageDownloader.downloadOutages(run);
 		logger.info("Finished download.");
-		for (AbstractOutageRevision revision : revisions) {
-			dao.updateOutage(revision);
-		}
+		dao.updateOutages(revisions);
 		dao.closeMissingOutages(
 				Collections2.transform(revisions, REVISION_TO_ID),
 				run.getAsof());
